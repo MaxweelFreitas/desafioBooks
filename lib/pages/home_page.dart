c318f5983545836484b13b5d8b3c529d791409f6
MaxweelFreitas <maxweel.freitas@gmail.com>
2020-03-13 03:45:49 -0300

    upload imgs2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:desafio_books/models/book.dart';
 import 'package:flutter/material.dart';
 
+import 'package:cached_network_image/cached_network_image.dart';
+
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
 
@@ -11,35 +13,57 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-
+    var size = MediaQuery.of(context).size;
     List<Book> listH = [
       Book(
-        title:'Harry Potter and the Goblet of Fire',
-        author:'J.K. Rowling',
-        price:19.99,
-        rate:4.8,
+        imgPath:
+            'https://github.com/MaxweelFreitas/desafioBooks/blob/master/imgs/harry_potter.png',
+        title: 'Harry Potter and the Goblet of Fire',
+        author: 'J.K. Rowling',
+        price: 19.99,
+        rate: 4.8,
         vote: 2390,
       ),
       Book(
-        title:'The Jungle Book',
-        author:'Rudyard Kipling',
-        price:19.99,
-        rate:4.8,
+        imgPath:
+            'https://github.com/MaxweelFreitas/desafioBooks/blob/master/imgs/jungle_book.png',
+        title: 'The Jungle Book',
+        author: 'Rudyard Kipling',
+        price: 19.99,
+        rate: 4.8,
         vote: 2390,
       ),
       Book(
-        title:'Star Wars Return of the Jedi',
-        author:'George Lucas',
-        price:19.99,
-        rate:4.8,
+        imgPath:
+            'https://github.com/MaxweelFreitas/desafioBooks/blob/master/imgs/starwars.png',
+        title: 'Star Wars Return of the Jedi',
+        author: 'George Lucas',
+        price: 19.99,
+        rate: 4.8,
         vote: 2390,
       ),
     ];
 
-    ListView listViewH = ListView.builder(
-      itemCount: 1,
+    var listViewH = ListView.builder(
+      shrinkWrap: true,
+      scrollDirection: Axis.horizontal,
+      itemCount: listH.length,
       itemBuilder: (BuildContext context, int index) {
-        return;
+
+        Book book = listH[index];
+
+        return Container(
+          height: 100,
+          width: 100,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: CachedNetworkImage(
+              imageUrl: book.imgPath,
+              placeholder: (context, url) => CircularProgressIndicator(),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+            ),
+          ),
+        );
       },
     );
 
@@ -63,14 +87,20 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
-        body: Container(
-          height: MediaQuery.of(context).size.height,
-          width: double.infinity,
-          child: Column(
-            children: <Widget>[
-              listViewH,
-            ],
-          ),
+        body: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Flexible(
+              flex: 1,
+              child: listViewH,
+            ),
+            Flexible(
+              flex: 5,
+              child: Container(
+                height: double.infinity,
+              ),
+            ),
+          ],
         ),
       ),
     );
